ec72f0c9231d5ceeb72f152cfdbc5b198f63b45e
WhiteBryz <davidflores@live.com.mx>
2026-09-17 12:47:02 -0600

    chore: remove some elements for the comments

--- routes/mqttRoutes.js
+++ routes/mqttRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const mqttService = require('../services/mqttService');
 const mqttController = require('../controllers/mqttController');
 
-// Ruta para obtener todos los mensajes recibidos de MQTT (si los guardas en Firebase)
+// Ruta para obtener todos los mensajes recibidos de MQTT
 router.get('/messages', mqttController.getMessages);
 
 // Ruta para publicar un mensaje en un topic MQTT
 router.post('/publish', mqttController.publishMessage);
 
-// Ruta para suscribirse a un nuevo topic desde la API (opcional)
+// Ruta para suscribirse a un nuevo topic desde la API 
 router.post('/subscribe', mqttController.subscribeToTopic);
